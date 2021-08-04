5612299cbb67714089b46d1ef69adf290c6e6ada
Slavko_V <slavko.vuletic92@gmail.com>
2021-08-04 14:49:10 +0200

    yes fix minor

--- tech_demo_v.01/index.js
+++ tech_demo_v.01/index.js
@@ -1,7 +1,7 @@
 //111111111
 let v_uni = require("v_uni");
 
-console.log("-[1-S]-[ v_uni ]<= Unicode Printing From Custom Node Module =>-------------------------------\n");
+console.log("\n-[1-S]-[ v_uni ]<= Unicode Printing From Custom Node Module =>-------------------------------\n");
 console.log(`=> v_uni.ok : ${v_uni.ok}`);
 console.log(`=> v_uni.tu_o : ${v_uni.tu_o}`);
 console.log(`=> v_uni.tx : ${v_uni.tx}`);
@@ -29,12 +29,12 @@ console.log(v_math.mul(99, 11));
 console.log("\n-[3-E]-[End]----------------------------------------------------------------------[End]-----\n\n\n");
 //444444444
 console.log("-[4-S]-[ v_uni + v_math  ]<= short if v_math.add, to print v_uni.ok or v_uni.name_block =>----------------\n");
-console.log(v_math.add(99, 11) == 110 ? v_uni.ok + " Ok, 110." : v_uni.bomb + " Nope Bad Job" );
-console.log(v_math.div(99, 9) == 11 ? v_uni.ok + " Yes it is. 11." : v_uni.bomb + " Nope Bad Job" );
-console.log(v_math.mul(99, 11) == 1089 ? v_uni.ok + " Yes 1089." : v_uni.bomb + " Nope Bad Job" );
-console.log(v_math.sub(99, 11) == 88 ? v_uni.ok + " Yup, 88." : v_uni.bomb + " Nope Bad Job" );
-console.log(v_math.add(99, 11) == 77 ? v_uni.ok + " Ok, 110." : v_uni.bomb + " Nope, far from 77" );
-console.log(v_math.div(99, 9) == 33 ? v_uni.ok + " Yes it is. 11." : v_uni.bomb + " Not 33." );
-console.log(v_math.mul(99, 11) == 11 ? v_uni.ok + " Yes 1089." : v_uni.bomb + " No, surely not 11." );
-console.log(v_math.sub(99, 11) == 124 ? v_uni.ok + " Yup, 88." : v_uni.bomb + " Nope it's not 124." );
+console.log(v_math.add(99, 11) == 110 ? v_uni.hdpt + " - Ok, 110." : v_uni.warning + " - Nope Bad Job" );
+console.log(v_math.div(99, 9) == 11 ? v_uni.hdpt + " - Yes it is. 11." : v_uni.warning + " - Nope Bad Job" );
+console.log(v_math.mul(99, 11) == 1089 ? v_uni.hdpt + " - Yes 1089." : v_uni.warning + " - Nope Bad Job" );
+console.log(v_math.sub(99, 11) == 88 ? v_uni.hdpt + " - Yup, 88." : v_uni.warning + " - Nope Bad Job" );
+console.log(v_math.add(99, 11) == 77 ? v_uni.hdpt + " - Ok, 110." : v_uni.warning + " - Nope, far from 77" );
+console.log(v_math.div(99, 9) == 33 ? v_uni.hdpt + " - Yes it is. 11." : v_uni.warning + " - Not 33." );
+console.log(v_math.mul(99, 11) == 11 ? v_uni.hdpt + " - Yes 1089." : v_uni.warning + " - No, surely not 11." );
+console.log(v_math.sub(99, 11) == 124 ? v_uni.hdpt + " - Yup, 88." : v_uni.warning + " - Nope it's not 124." );
 console.log("\n-[4-E]-[End]----------------------------------------------------------------------[End]-----\n\n\n");
