060dba4bac0961526143f0f28f2ac82ab07acfbf
Slavko_V <slavko.vuletic92@gmail.com>
2021-08-10 14:42:24 +0200

    yea old meets the new

--- [_.V._]_Modules/V_DOM/V_DOM.js
+++ [_.V._]_Modules/V_DOM/V_DOM.js
@@ -27,7 +27,33 @@ const v_dom = {
         },
         hero_v (title="Empty HERO_V Title Space", subtitle="SubTitle Text Space for HERO_V", text="Nightwatch.js was created in Oslo, Norway by Pineview.io – an independent software consultancy; it is now being maintained by all these fine people with help from all our contributors.", imgUrl="https://nightwatchjs.org/images/symbol-nightwatch.svg") {
             return `<v_hero>${this.h2(title)}${this.h5(subtitle)}${this.p(text)}${this.img(imgUrl)}</v_hero>`;
-        }
+        },
+				checksection_base01 (){
+					return `
+					<div class="customFormInputField customCheckBox">
+						<div class="sectionInfo">
+							<div class="icon"><i class="fa fa-superpowers" aria-hidden="true"></i></div>
+							<div class="headingText">Some Sample Setting</div>
+							<div class="descriptionText">Yes we will write description inside this somehow it should be able to show it even if the text gets bigger or contains a <a href="#">link</a></div>
+						</div>
+						<div class="checkBoxContainer checked">
+							<div class="checkBoxOutter">
+								<div class="checkBoxInner"></div>
+							</div>
+						</div>
+					</div>`;
+				},
+				v_magic_login(){
+					return `<form>
+										<input id="secret_code" type="text" placeholder="#14mvnc^&sc0123" required  />
+										<label for="secret_code">
+											<span class="label-text">Secret Login Code:</span>
+											<span class="nav-dot"></span>
+										</label>>
+										<button type="submit">Login</button>
+										<p class="tip">Press Tab</p>
+									</form>`;
+				}
     },
 		register_block(blockParams = null){
 			console.log("REGISTER BLOCK: "+blockParams);
